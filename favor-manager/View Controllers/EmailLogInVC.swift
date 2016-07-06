56016b45316280cab7cd9c4881462b14e997f8e9
joe-broder15 <joey.broder@gmail.com>
2016-07-06 15:04:40 -0700

    UI Changes
    Added the login and welcome screen

---
+++ favor-manager/View Controllers/EmailLogInVC.swift
@@ -0,0 +1,19 @@
+//
+//  EmailLogInVC.swift
+//  favor-manager
+//
+//  Created by JoeB on 7/6/16.
+//  Copyright © 2016 Vyacheslav Horbach. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EmailLogInVC: UIViewController{
+    
+    
+    @IBAction func backButton(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
