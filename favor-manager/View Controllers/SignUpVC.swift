f8a083c289a80fc885b0b3b1c164f19535762fbc
joe-broder15 <joey.broder@gmail.com>
2016-07-06 15:27:51 -0700

    added sign up screen

---
+++ favor-manager/View Controllers/SignUpVC.swift
@@ -0,0 +1,22 @@
+//
+//  SignUpVC.swift
+//  favor-manager
+//
+//  Created by JoeB on 7/6/16.
+//  Copyright © 2016 Vyacheslav Horbach. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SignUpVC: UIViewController {
+    
+    @IBAction func enterButtonPressed(sender: UIButton) {
+        
+    }
+    
+    @IBAction func backButtonapped(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
