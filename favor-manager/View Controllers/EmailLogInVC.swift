a843d72b7e05100231f2a48024416e3ce4cc5481
Slavik <vjacheslav.horbach@gmail.com>
2016-07-07 11:12:25 -0700

    email login working

--- favor-manager/View Controllers/EmailLogInVC.swift
+++ favor-manager/View Controllers/EmailLogInVC.swift
@@ -8,8 +8,28 @@
 
 import Foundation
 import UIKit
+import Firebase
 
 class EmailLogInVC: UIViewController{
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBAction func enterButtonPressed(sender: UIButton) {
+        if emailTextField.text != "" && passwordTextField.text != "" {
+            
+            FIRAuth.auth()?.signInWithEmail(emailTextField.text!, password: passwordTextField.text!, completion: { (user, err) in
+                if let user = user {
+                    print("user is in!!!! \(user)")
+                    self.performSegueWithIdentifier("showFavorsVCfromLogIn", sender: self)
+                } else {
+                    print(err.debugDescription)
+                }
+            })
+            
+        }
+    }
     
     
     @IBAction func backButton(sender: UIButton) {
