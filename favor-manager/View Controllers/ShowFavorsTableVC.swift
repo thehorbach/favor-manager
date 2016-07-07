928e2b038c0e3d4d6b8ecbbefb0fb364fb025666
joe-broder15 <joey.broder@gmail.com>
2016-07-07 16:40:42 -0700

    Table View Now Works
    I have the basis working, now we just need to populate in data

--- favor-manager/View Controllers/ShowFavorsTableVC.swift
+++ favor-manager/View Controllers/ShowFavorsTableVC.swift
@@ -13,6 +13,7 @@ class ShowFavorsTableVC: UIViewController {
     @IBOutlet weak var personName: UILabel!
     @IBOutlet weak var favorName: UILabel!
     @IBOutlet weak var dueDate: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
@@ -33,10 +34,29 @@ class ShowFavorsTableVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
+extension ShowFavorsTableVC: UITableViewDelegate, UITableViewDataSource {
+    
     // MARK: - Table view data source
     
-
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
     
+    //THIS WILL CHANGE
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    //THIS WILL ALSO CHANGE WHEN WE GET DATA
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("favorCell", forIndexPath: indexPath)
+//        let post = posts[indexPath.row]
+//        cell.textLabel?.text = "\(post.item1!) + \(post.item2!)"
+//        cell.detailTextLabel?.text = "Likes: \(post.likes)"
+        
+        return cell
+    }
 
 }
